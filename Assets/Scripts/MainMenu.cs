7f20365e71c4027a2122b407127893a9b07e3bbc
oscar22906 <544278@chrome.escg.ac.uk>
2023-11-30 16:08:00 +0000

    Initial commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] AudioSource buttonPressSound;
+    public Animator animator;
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+    public void Settings()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void PlayGame()
+    {
+        Invoke("LoadGame", 2);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/SoundBar.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Slider volumeSlider;
+
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    // Update is called once per frame
+    public void ChangeVolume()
+    {
+
+        AudioListener.volume = volumeSlider.value;
+        Save();
+
+    }
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+
+}

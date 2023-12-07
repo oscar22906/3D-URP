f49fc9f32276f882992ecd9ed95797fc39222451
oscar22906 <544278@chrome.escg.ac.uk>
2023-12-07 20:53:56 +0000

    amazing version
    very best of the versions for now,
    bugs - gun aim no working

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -14,11 +14,6 @@ namespace StarterAssets
 #endif
     public class ThirdPersonController : MonoBehaviour
     {
-        // Reference to the animator component.
-        Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
-
-        int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
-        float camRayLength = 100f;          // The length of the ray from the camera into the scene.
 
         [Header("Player")]
         [Tooltip("Move speed of the character in m/s")]
@@ -131,11 +126,6 @@ namespace StarterAssets
 
         private void Awake()
         {
-            // Create a layer mask for the floor layer.
-            floorMask = LayerMask.GetMask("Floor");
-
-            // Set up references.
-            playerRigidbody = GetComponent<Rigidbody>();
 
             // get a reference to our main camera
             if (_mainCamera == null)
@@ -168,7 +158,7 @@ namespace StarterAssets
         {
             _hasAnimator = TryGetComponent(out _animator);
             // Turn the player to face the mouse cursor.
-            Turning();
+
             JumpAndGravity();
             GroundedCheck();
             Move();
@@ -223,30 +213,7 @@ namespace StarterAssets
             CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride,
                 _cinemachineTargetYaw, 0.0f);
         }
-        void Turning()
-        {
-            // Create a ray from the mouse cursor on screen in the direction of the camera.
-            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            // Create a RaycastHit variable to store information about what was hit by the ray.
-            RaycastHit floorHit;
-
-            // Perform the raycast and if it hits something on the floor layer...
-            if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
-            {
-                // Create a vector from the player to the point on the floor the raycast from the mouse hit.
-                Vector3 playerToMouse = floorHit.point - transform.position;
-
-                // Ensure the vector is entirely along the floor plane.
-                playerToMouse.y = 0f;
-
-                // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
-                Quaternion newRotatation = Quaternion.LookRotation(playerToMouse);
-
-                // Set the player's rotation to this new rotation.
-                playerRigidbody.MoveRotation(newRotatation);
-            }
-        }
         private void Move()
         {
             // set target speed based on move speed, sprint speed and if sprint is pressed
